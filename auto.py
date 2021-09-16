3798c810a0ee308517faacdf6e7156278450f423
sydbermas <sydbermas@gmail.com>
2021-09-16 15:20:47 +0800

    added-bot-unfinished2

--- auto.py
+++ auto.py
@@ -1,42 +1,105 @@
-import pyautogui
-import glob
+from tkinter.constants import LEFT
 import cv2
+import pyautogui
+import os, time
+import win32gui, win32con
 
-
-def bot():
-    pyautogui.screenshot('screenshot.jpg')  
-    imageToBeInspected = 'screenshot.jpg'  
-    poms = [glob.glob('7W82995\A\\*.jpg')]
-
-    for pom in poms:
-
-        img = cv2.imread(imageToBeInspected,0)
-        img2 = img.copy()
+def imgAutoCick(tempFile, whatDo, debug=False):
+    '''
+        temFile :The small picture that needs to be matched
+        whatDo  :Required action
+                pyautogui.moveTo(w/2, h/2)# Basic movement
+                pyautogui.leftClick()  # Left click
+                pyautogui.dragTo()  # drag mouse to X of n, Y of n while holding down left mouse button
+                pyautogui.drag(w/2, h/2)
+                pyautogui._mouseMoveDrag(w/2, h/2)
+                pyautogui.mouseUp()
+                pyautogui.mouseDown(w/2, h/2, 1, 0, 2)
+                pyautogui.doubleClick(w/2, h/2,)  # Double left click
+                pyautogui.click()
+                pyautogui.rightClick(w/2, h/2) # Right-click
+                pyautogui.middleClick() # Middle click
+                pyautogui.tripleClick() # Mouse current position 3 clicks
+                pyautogui.scroll(10) # Roll the wheel up by 10, pay attention to the direction, and go down for negative values
         
-        templAB = cv2.imread(pom[0],0) # pom start
-        templBA = cv2.imread(pom[1],0)
-        w, h = templAB.shape[::-1]
-        w2, h2 = templBA.shape[::-1]
-        # All the 6 methods for comparison in a list
-        methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
-                'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
-        meth = methods[1] # Mehtod setting
-        img = img2.copy()
-        methodAB = eval(meth)
-        # Apply template Matching
-        res = cv2.matchTemplate(img,templAB,methodAB)
-        res2 = cv2.matchTemplate(img,templBA,methodAB)
-        
-        _, _, min_loc, max_loc = cv2.minMaxLoc(res)
-        _, _, min_loc2, max_loc2 = cv2.minMaxLoc(res2)
-        
-        # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-        if methodAB in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-            top_left = min_loc
-            top_left2 = min_loc2
+    More details：https://blog.csdn.net/weixin_43430036/article/details/84650938
+        debug   :Whether to open the display debugging window
+    '''
+ 
+    pyautogui.screenshot('screenshot.png')
 
-        else:
-            Btl = max_loc
-            Btl2 = max_loc2
-    print(Btl[0],Btl[1])
-    pyautogui.move(Btl[0],Btl[1],0.5)
+ 
+    gray = cv2.imread("screenshot.png",0)
+ 
+    img_template = cv2.imread(tempFile,0)
+
+    w, h = img_template.shape[::-1]
+
+    res = cv2.matchTemplate(gray,img_template,cv2.TM_SQDIFF)
+
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+    top = min_loc[0]
+    left = min_loc[1]
+    x = [top, left, w, h]
+
+    top_left = min_loc
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+
+    pyautogui.moveTo(top+h/2, left+w/2)
+    whatDo(x)
+
+    # if debug:
+
+    #     img = cv2.imread("screenshot.png",1)
+
+    #     cv2.rectangle(img,top_left, bottom_right, (0,0,255), 2)
+
+    #     img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5, interpolation=cv2.INTER_NEAREST)
+    #     cv2.imshow("processed",img)
+    #     cv2.waitKey(0)
+
+    #     cv2.destroyAllWindows()
+    # os.remove("screenshot.png")
+
+# def simple_example_baidu():
+  
+#     # time.sleep(2)
+#     # imgAutoCick("7W82995\A\\b.jpg", pyautogui.dragTo)
+
+#     # time.sleep(2)
+#     # imgAutoCick("7W82995\A\\b.jpg", pyautogui.mouseUp)
+
+#     # time.sleep(1)
+#     # pyautogui.typewrite("meinv1")
+
+#     # time.sleep(1)
+#     # imgAutoCick("baidu_tu/test4.png", pyautogui.click)
+ 
+#     # time.sleep(1)
+#     # imgAutoCick("baidu_tu/test5.png", pyautogui.click)
+
+#     m, n = pyautogui.size()
+#     pyautogui.moveTo(x=m/2, y=n/2)
+
+#     for i in range(100):
+#         time.sleep(0.5)
+#         pyautogui.scroll(-120) 
+
+if __name__=="__main__":
+  
+    # Minimize = win32gui.GetForegroundWindow()
+    # win32gui.ShowWindow(Minimize, win32con.SW_MINIMIZE)
+
+    # pyautogui.PAUSE = 1
+    # pyautogui.press('winleft')
+    # pyautogui.PAUSE = 1
+
+    imgAutoCick("7W82995\A\\a.jpg", pyautogui.doubleClick)
+    imgAutoCick("7W82995\A\\b.jpg", pyautogui.rightClick)
+    time.sleep(0.2)
+
+        
+    
+    
+    
--- mousedrawevent.py
+++ mousedrawevent.py
@@ -20,25 +20,26 @@ class DrawLineWidget(object):
 
     def extract_coordinates(self, event, x, y, flags, parameters):
         # Record starting (x,y) coordinates on left mouse button click
-        if event == cv2.EVENT_LBUTTONDOWN:
+        if event == cv2.EVENT_LBUTTONDBLCLK:
             self.image_coordinates = [(x,y)]
             
 
         # Record ending (x,y) coordintes on left mouse bottom release
-        elif event == cv2.EVENT_LBUTTONUP:
+        elif event == cv2.EVENT_RBUTTONDOWN:
             self.image_coordinates.append((x,y))
             print('Starting: {}, Ending: {}'.format(self.image_coordinates[0], self.image_coordinates[1]))
             distA = math.sqrt((self.image_coordinates[1][0] -self.image_coordinates[0][0])**2 + (self.image_coordinates[1][1] -self.image_coordinates[0][1])**2)
-            A = (distA/14.5)
-            print("A:"+str(round(A,2))+"inches")
+            dist = (distA/14.5)
+            print("A:"+str(round(dist,2))+"inches")
 
             # Draw line
-            cv2.line(self.clone, self.image_coordinates[0], self.image_coordinates[1], (0,0,0), 2)
+            cv2.line(self.clone, self.image_coordinates[0], self.image_coordinates[1], (0,0,0), 1)
+            cv2.putText(self.clone,("A:"+str(round(dist,2))+"inches"),(self.image_coordinates[0]),cv2.FONT_HERSHEY_SIMPLEX,0.4,(0,0,0),1)
             cv2.imshow("image", self.clone)
              
 
         # Clear drawing boxes on right mouse button click
-        elif event == cv2.EVENT_RBUTTONDOWN:
+        elif event == cv2.EVENT_LBUTTONDBLCLK:
             self.clone = self.original_image.copy()
             
     def show_image(self):
@@ -55,10 +56,9 @@ if __name__ == '__main__':
         key = cv2.waitKey(1)
         
         if key == ord(' '):
-            auto.bot()
+            exec(open('auto.py').read())
         # Close program with keyboard 'q'
-        if key == ord('q'):
-            auto.bot()
+        if key == ord('q'): 
             cv2.destroyAllWindows()
             exit(1)
 
